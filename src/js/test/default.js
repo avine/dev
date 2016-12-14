167619bcc2244895bda8f738e5a8c90fef89e584
Stéphane.francel <s.francel@avine.fr>
2016-12-15 00:26:03 +0100

    Add tests

---
+++ src/js/test/default.js
@@ -0,0 +1,13 @@
+/* eslint-env mocha */
+'use strict';
+
+import chai from 'chai';
+var expect = chai.expect;
+
+describe('Mocha and Chai', function () {
+
+  it('should be ok', function () {
+    expect(true).to.be.true;
+  });
+
+});
