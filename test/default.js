fa312743f538b88cf5bb762a05772ea4fc68f9ce
avine <s.francel@avine.fr>
2016-12-14 10:48:02 +0100

    tool.typeOf tests suite

--- test/default.js
+++ test/default.js
@@ -2,7 +2,7 @@
 
 var expect = require('chai').expect;
 
-describe('Mocha and Chai are ready!', function () {
+describe('Mocha and Chai', function () {
 
   it('should be ok', function () {
     expect(true).to.be.true;
